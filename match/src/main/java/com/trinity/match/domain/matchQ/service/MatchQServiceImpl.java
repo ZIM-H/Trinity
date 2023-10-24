e07497892eae7dfd62eca8ae3a99e8b507f6cde4
송혁준 <thdgurws98@gmail.com>
2023-10-24 17:56:31 +0900

    [feat] controller 구현 231024 1756

--- match/src/main/java/com/trinity/match/domain/matchQ/service/MatchQServiceImpl.java
+++ match/src/main/java/com/trinity/match/domain/matchQ/service/MatchQServiceImpl.java
@@ -1,6 +1,8 @@
 package com.trinity.match.domain.matchQ.service;
 
 import com.trinity.match.domain.matchQ.service.MatchQService;
+import org.springframework.stereotype.Service;
 
+@Service
 public class MatchQServiceImpl implements MatchQService {
 }
