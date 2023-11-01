3544770b0e23f1a5139238417278c3831f4474e9
송혁준 <thdgurws98@gmail.com>
2023-11-01 10:24:40 +0900

    [feat] create game room 231101 1024

--- trinity/src/main/java/com/trinity/trinity/gameRoom/dto/FirstRoom.java
+++ trinity/src/main/java/com/trinity/trinity/gameRoom/dto/FirstRoom.java
@@ -2,21 +2,25 @@ package com.trinity.trinity.gameRoom.dto;
 
 import lombok.Builder;
 import lombok.Getter;
+import lombok.NoArgsConstructor;
 
 @Getter
+@NoArgsConstructor
 public class FirstRoom {
     private int fertilizerAmount;
     private String player;
     private String message;
     private int fertilizerUpgradeStatus;
     private int purifierStatus;
+    private int inputFertilizer;
 
     @Builder
-    public FirstRoom(int fertilizerAmount, String player, String message, int fertilizerUpgradeStatus, int purifierStatus) {
+    public FirstRoom(int fertilizerAmount, String player, String message, int fertilizerUpgradeStatus, int purifierStatus, int inputFertilizer) {
         this.fertilizerAmount = fertilizerAmount;
         this.player = player;
         this.message = message;
         this.fertilizerUpgradeStatus = fertilizerUpgradeStatus;
         this.purifierStatus = purifierStatus;
+        this.inputFertilizer = inputFertilizer;
     }
 }
