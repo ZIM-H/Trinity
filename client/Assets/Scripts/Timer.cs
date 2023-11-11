88ff1f6433fa72a77932d1a986976a619ec0ac06
Junyoung Heo <junyoung449@gmail.com>
2023-11-11 23:39:15 +0900

    [fix]merge conflict 해결 231111 1139

--- client/Assets/Scripts/Timer.cs
+++ client/Assets/Scripts/Timer.cs
@@ -25,12 +25,27 @@ public class Timer : MonoBehaviour
     IEnumerator ExecuteAfterTime(float time)
     {
         yield return new WaitForSeconds(time);
-        
-        int roomNo = VariableManager.Instance.roomNo;
-        string room = roomNames[roomNo];
-        Debug.Log("여기부터 : "+roomNo);
-        Debug.Log("여기부터 : "+room);
-        
-        SceneManager.LoadScene(room);
+        if (VariableManager.Instance.gameOver) {
+            // 다른 스크립트에서 WebSocketClientManager의 인스턴스를 얻어옴
+            WebSocketClientManager webSocketManager = FindObjectOfType<WebSocketClientManager>();
+
+            // WebSocketClientManager의 인스턴스가 존재하는지 확인
+            if (webSocketManager != null)
+            {
+                // toggleWebSocketConnection 함수 호출
+                webSocketManager.ToggleWebSocketConnection();
+            }
+            else
+            {
+                Debug.LogError("WebSocketClientManager not found in the scene.");
+            }
+        }
+        else {
+            int roomNo = VariableManager.Instance.roomNo;
+            string room = roomNames[roomNo];
+            Debug.Log("여기부터 : "+roomNo);
+            Debug.Log("여기부터 : "+room);
+            SceneManager.LoadScene(room);
+        }
     }
 }
