a7a13068e7b4ef11556573c72076d93da571a5ee
송혁준 <thdgurws98@gmail.com>
2023-11-14 14:19:31 +0900

    [fix] match server 231114 1419

--- match/src/main/java/com/trinity/match/domain/matchQ/service/MatchQServiceImpl.java
+++ match/src/main/java/com/trinity/match/domain/matchQ/service/MatchQServiceImpl.java
@@ -28,146 +28,61 @@ public class MatchQServiceImpl implements MatchQService {
 
     private static final String LOCK_NAME = "matchQueueLock";
 
-//    @Override
-//    public boolean joinQueue(String userId) {
-//        RLock lock = matchRedissonClient.getLock(LOCK_NAME);
-//        lock.lock();
-//        try {
-//            return redisService.addUser(userId);
-//        } catch (Exception e) {
-//            log.error(e.getMessage());
-//            return false;
-//        } finally {
-//            lock.unlock();
-//        }
-//    }
-
-//    @Scheduled(fixedRate = 2000)
-//    private void checkQueueSize() {
-//        RLock lock = matchRedissonClient.getLock(LOCK_NAME);
-//        lock.lock();
-//        List<Pair<String, Double>> waitingList = new ArrayList<>();
-//        try {
-//            // 대기 큐의 크기가 3 보다 작으면 그만
-//            if (redisService.getSize() < 3) return;
-//
-//            while (waitingList.size() != 3) {
-//                // 1순위 사람 뽑기
-//                Set<ZSetOperations.TypedTuple<String>> rangeWithScores = redisService.getSet();
-//                if (rangeWithScores == null || rangeWithScores.isEmpty()) break;
-//
-//                ZSetOperations.TypedTuple<String> next = rangeWithScores.iterator().next();
-//                String findUserId = next.getValue();
-//                Double score = next.getScore();
-//
-//                // 게임 서버 redis에 접근해 유효성 검사
-//                Object state = redisService.validate(findUserId);
-//                if (state != null && state.toString().equals("WAITING")) {
-//                    waitingList.add(Pair.of(findUserId, score));
-//                }
-//                redisService.deleteData(findUserId);
-//            }
-//
-//            // 게임 서버에 보낼 리스트의 크기가 3보다 작으면 다시 대기큐에 넣고 돌아가기
-//            if (waitingList.size() < 3) {
-//                redisService.recoverList(waitingList);
-//                return;
-//            }
-//
-//            for (Pair<String, Double> userAndScore : waitingList) {
-//                redisService.deleteData(userAndScore.getFirst());
-//            }
-//
-//            List<GameServerPlayerListRequestDto> playerList = new ArrayList<>();
-//            for (Pair<String, Double> userAndScore : waitingList)
-//                playerList.add(GameServerPlayerListRequestDto.builder()
-//                        .userId(userAndScore.getFirst())
-//                        .build());
-//
-//            webClientService.post(playerList, waitingList);
-//
-//        } catch (Exception e) {
-//            // 에러 발생하면 에러 메시지 찍고 대기 큐에 다시 넣기
-//            log.error(e.getMessage());
-//            redisService.recoverList(waitingList);
-//        } finally {
-//            lock.unlock();
-//        }
-//    }
-
-    static PriorityQueue<Player> pq = new PriorityQueue<Player>((o1, o2) -> Double.compare(o1.time, o2.time));
-
-    public static class Player{
-        String userId;
-        double time;
-
-        public Player(String userId, double time) {
-            this.userId = userId;
-            this.time = time;
-        }
-    }
-
+    // ===============================================
     @Override
     public boolean joinQueue(String userId) {
-        double time = System.currentTimeMillis();
-        Player player = new Player(userId, time);
-        pq.offer(player);
-
+        RLock lock = matchRedissonClient.getLock(LOCK_NAME);
+        lock.lock();
         try {
-            log.info("pq.size() : " + pq.size());
-            if (pq.size() >= 3) {
-                log.info("pq.size()가 3 이상인 경우만");
-                checkQueueSize();
-            }
+            return redisService.addUser(userId);
         } catch (Exception e) {
-            log.info(e.getMessage());
+            log.error(e.getMessage());
             return false;
+        } finally {
+            lock.unlock();
         }
-
-        return true;
     }
 
+    @Scheduled(fixedRate = 2000)
     private void checkQueueSize() {
         RLock lock = matchRedissonClient.getLock(LOCK_NAME);
         lock.lock();
-
-        List<Player> waitingList = new ArrayList<>();
+        List<Pair<String, Double>> waitingList = new ArrayList<>();
         try {
-            Thread.sleep(2000);
-            
+            // 대기 큐의 크기가 3 보다 작으면 그만
+            if (redisService.getSize() < 3) return;
+
             while (waitingList.size() != 3) {
-                if (pq.size() == 0) break;
-
                 // 1순위 사람 뽑기
-                Player findPlayer = pq.poll();
+                Set<ZSetOperations.TypedTuple<String>> rangeWithScores = redisService.getSet();
+                if (rangeWithScores == null || rangeWithScores.isEmpty()) break;
 
-                Object state = redisService.validate(findPlayer.userId);
+                ZSetOperations.TypedTuple<String> next = rangeWithScores.iterator().next();
+                String findUserId = next.getValue();
+                Double score = next.getScore();
 
-                log.info("userId : " + findPlayer.userId + " " + state.toString());
-
+                // 게임 서버 redis에 접근해 유효성 검사
+                Object state = redisService.validate(findUserId);
                 if (state != null && state.toString().equals("WAITING")) {
-                    log.info("waitingList에 삽입");
-                    waitingList.add(findPlayer);
+                    waitingList.add(Pair.of(findUserId, score));
                 }
+                redisService.deleteData(findUserId);
             }
-
-            log.info("waitingList.size() : " + waitingList.size());
 
             // 게임 서버에 보낼 리스트의 크기가 3보다 작으면 다시 대기큐에 넣고 돌아가기
             if (waitingList.size() < 3) {
-                log.info("복구 전 pq.size() : " + pq.size());
-
-                for (Player player : waitingList) pq.offer(player);
-
-                log.info("복구 후 pq.size() : " + pq.size());
-
+                redisService.recoverList(waitingList);
                 return;
             }
 
+            for (Pair<String, Double> userAndScore : waitingList) {
+                redisService.deleteData(userAndScore.getFirst());
+            }
+
             List<GameServerPlayerListRequestDto> playerList = new ArrayList<>();
-            for (Player player : waitingList)
+            for (Pair<String, Double> userAndScore : waitingList)
                 playerList.add(GameServerPlayerListRequestDto.builder()
-                        .userId(player.userId)
+                        .userId(userAndScore.getFirst())
                         .build());
 
             webClientService.post(playerList, waitingList);
@@ -175,10 +90,99 @@ public class MatchQServiceImpl implements MatchQService {
         } catch (Exception e) {
             // 에러 발생하면 에러 메시지 찍고 대기 큐에 다시 넣기
             log.error(e.getMessage());
-//            recoverList(waitingList);
-            for (Player player : waitingList) pq.offer(player);
+            redisService.recoverList(waitingList);
         } finally {
             lock.unlock();
         }
     }
+
+
+
+    // ====================================================================
+//    static PriorityQueue<Player> pq = new PriorityQueue<Player>((o1, o2) -> Double.compare(o1.time, o2.time));
+//
+//    public static class Player{
+//        String userId;
+//        double time;
+//
+//        public Player(String userId, double time) {
+//            this.userId = userId;
+//            this.time = time;
+//        }
+//    }
+//
+//    @Override
+//    public boolean joinQueue(String userId) {
+//        double time = System.currentTimeMillis();
+//        Player player = new Player(userId, time);
+//        pq.offer(player);
+//
+//        try {
+//            log.info("pq.size() : " + pq.size());
+//            if (pq.size() >= 3) {
+//                log.info("pq.size()가 3 이상인 경우만");
+//                checkQueueSize();
+//            }
+//        } catch (Exception e) {
+//            log.info(e.getMessage());
+//            return false;
+//        }
+//
+//        return true;
+//    }
+//
+//    private void checkQueueSize() {
+//        RLock lock = matchRedissonClient.getLock(LOCK_NAME);
+//        lock.lock();
+//
+//        List<Player> waitingList = new ArrayList<>();
+//        try {
+//            Thread.sleep(2000);
+//
+//            while (waitingList.size() != 3) {
+//                if (pq.size() == 0) break;
+//
+//                // 1순위 사람 뽑기
+//                Player findPlayer = pq.poll();
+//
+//                Object state = redisService.validate(findPlayer.userId);
+//
+//                log.info("userId : " + findPlayer.userId + " " + state.toString());
+//
+//                if (state != null && state.toString().equals("WAITING")) {
+//                    log.info("waitingList에 삽입");
+//                    waitingList.add(findPlayer);
+//                }
+//            }
+//
+//            log.info("waitingList.size() : " + waitingList.size());
+//
+//            // 게임 서버에 보낼 리스트의 크기가 3보다 작으면 다시 대기큐에 넣고 돌아가기
+//            if (waitingList.size() < 3) {
+//                log.info("복구 전 pq.size() : " + pq.size());
+//
+//                for (Player player : waitingList) pq.offer(player);
+//
+//                log.info("복구 후 pq.size() : " + pq.size());
+//
+//                return;
+//            }
+//
+//            List<GameServerPlayerListRequestDto> playerList = new ArrayList<>();
+//            for (Player player : waitingList)
+//                playerList.add(GameServerPlayerListRequestDto.builder()
+//                        .userId(player.userId)
+//                        .build());
+//
+//            webClientService.post(playerList, waitingList);
+//
+//        } catch (Exception e) {
+//            // 에러 발생하면 에러 메시지 찍고 대기 큐에 다시 넣기
+//            log.error(e.getMessage());
+////            recoverList(waitingList);
+//            for (Player player : waitingList) pq.offer(player);
+//        } finally {
+//            lock.unlock();
+//        }
+//    }
 }
